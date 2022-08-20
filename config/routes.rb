0fde9594d269fde8008d5e20db7f2d6bb6109168
Jaime Montes de Oca <jaime0422@live.com>
2022-08-20 16:18:04 -0700

    Completed Assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+    # defining each of the routes manually
+    # get "/birds", to: "birds#index"     # show a list of all birds
+    # get "/birds/:id", to: "birds#show"  # show one a specific bird
+
+    # defining all the RESTful routes automatically
+    # resources :birds
+
+    # creating only specific routes
+    resources :birds, only: [:index, :show]
 end
